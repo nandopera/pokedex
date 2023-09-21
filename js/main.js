5e5496cb7ce7400afd29768d5d2038467f33734d
Fernando Costa Pereira <nandopera@gmail.com>
2023-09-21 00:51:02 -0300

    Divisão dos arquivos js, organização das listas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-const offset = 0
-const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
-
 function pokemonLi(pokemon) {
     return `
     <li class="pokemon">
@@ -18,9 +14,8 @@ function pokemonLi(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
 
-pokeApi.getPokemons().then((pokemons) => {
-    for (var i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i]
-        pokemonList.innerHTML += pokemonLi(pokemon)
-    }
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(pokemonLi).join('')
+    pokemonList.innerHTML = newHtml
+
 })
--- js/poke-api.js
+++ js/poke-api.js
@@ -1,9 +1,13 @@
-const pokeApi = {}
+const offset = 0;
+const limit = 10;
+const url = "https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}";
+
+const pokeApi = {};
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(url)
-      .then((response) => response.json())
-      .then((jsonBody) => jsonBody.results)
-      .catch((error) => console.error(error))
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .catch((error) => console.error(error));
 };
