a8da0a3533a5bae3d90b795932b7e845f306d1c3
Fernando Costa Pereira <nandopera@gmail.com>
2023-09-26 16:21:42 -0300

    New branch with change for details.

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@ const maxRecords = 151
 const limit = 10
 let offset = 0
 
+
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-      const newHtml = pokemons.map((pokemon) =>
+      const newHtml = pokemons
+        .map(
+          (pokemon) =>
             `
-            <li class="pokemon ${pokemon.type}">
+            <li class="pokemon-item pokemon ${pokemon.type}" data-id="${
+              pokemon.id
+            }">
                 <span class="number">#${pokemon.num}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
@@ -20,12 +26,67 @@ function loadPokemonItens(offset, limit) {
                           )
                           .join("")}
                     </ol>
+                      
                     <img src="${pokemon.photo}" alt="${pokemon.name}"></div>
-            </li>`
-        ).join("");
+            </li>
+            
+            `
+        )
+        .join("");
         pokemonList.innerHTML += newHtml
     });
 }
+const modal = document.querySelector("#modal");
+const closeModalButton = document.querySelector("#closeModal");
+const callPokeDetail = document.querySelector(".pokemon-item");
+// Adicione um manipulador de eventos ao elemento pai existente (pokemonList)
+/* pokemonList.addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'btn-detail') {
+    modal.showModal();
+  }
+}); */
+pokemonList.addEventListener("click", (e) => {
+  const clickedPokemonItem = e.target.closest(".pokemon-item");
+  if (clickedPokemonItem) {
+    modal.showModal();
+  }
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.close();
+});
+
+const modalTitle = document.querySelector("#modal h2");
+const modalDescription = document.querySelector("#modal p");
+
+// Função para carregar detalhes de um Pokémon no modal
+function loadPokemonDetails(pokemon) {
+  // Atualize o título do modal
+  modalTitle.textContent = pokemon.name;
+
+  // Atualize a descrição do modal
+  modalDescription.textContent = pokemon.description;
+
+  // Outras atualizações de elementos do modal, se necessário
+}
+
+function loadPokemonDetails(pokemonId) {
+  // Verifique se o Pokémon com o ID existe no objeto pokemonData
+  if (pokemonData[pokemonId]) {
+    const pokemon = pokemonData[pokemonId];
+
+    // Atualize o conteúdo do modal com os detalhes do Pokémon
+    modal.querySelector("h2").innerHTML = pokemon.name;
+    modal.querySelector("p").innerHTML = pokemon.description;
+    // Atualize outras informações do modal conforme necessário
+    
+
+    // Abra o modal
+    modal.showModal();
+  }
+}
+
+
 
 loadPokemonItens(offset, limit);
 
---
+++ js/poke-details.js
@@ -0,0 +1,6 @@
+/* const modal = document.querySelector('#modal');
+const callPokeDetail = document.querySelector('#btn-detail');
+
+callPokeDetail.addEventListener('click', (e) => {
+  modal.showModal();
+}) */
